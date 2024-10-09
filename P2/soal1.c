ef796e263ad10ada9ec3cdce96627115727fde5b
MaedaKenji <66903425+MaedaKenji@users.noreply.github.com>
2024-10-09 15:08:44 +0700

    update

---
+++ P2/soal1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Deklarasi array untuk menyimpan 5 angka
+    int angka[5];
+    
+    // Meminta input 5 angka dari pengguna
+    
+
+    // Memeriksa setiap angka apakah ganjil atau genap
+    
+    return 0;
+}
---
+++ P2/soal2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int angka[10];  // Array untuk menyimpan 10 angka
+    int positif = 0, negatif = 0, nol = 0;  // Variabel untuk menghitung jumlah positif, negatif, nol
+    int total = 0;  // Untuk menghitung jumlah semua angka
+    float rata_rata;  // Untuk menghitung rata-rata
+
+    // Meminta input 10 angka dari pengguna
+    
+
+    // Menghitung rata-rata
+    
+
+    // Menampilkan hasil perhitungan
+    printf("Jumlah angka positif:");
+    printf("Jumlah angka negatif:");
+    printf("Jumlah angka nol:");
+    printf("Rata-rata:");
+
+    // Menampilkan angka yang lebih besar dari rata-rata
+    printf("Angka yang lebih besar dari rata-rata: ");
+    
+
+    // Menampilkan angka yang lebih kecil dari rata-rata
+    printf("Angka yang lebih kecil dari rata-rata: ");
+
+    return 0;
+}
